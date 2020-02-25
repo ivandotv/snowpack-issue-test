049190adf266bc820f4290e2607479a7368a241a
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-02-25 21:00:44 +0100

    init

---
+++ dist/src/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _lodash = _interopRequireDefault(require("lodash.filter"));
+
+var _lodash2 = _interopRequireDefault(require("lodash.orderby"));
+
+var _mobx = _interopRequireDefault(require("/web_modules/mobx.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log(_mobx["default"]);
+console.log(_lodash["default"]);
+console.log(_lodash2["default"]);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import filter from "lodash.filter";
+import orderBy from "lodash.orderby";
+import mobx from "mobx";
+
+console.log(mobx);
+console.log(filter);
+console.log(orderBy);
